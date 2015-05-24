d34a2b4a13727a383e725cc49dd9028ce242e557
Siddhant Bansal <bansal.siddhant@gmail.com>
2015-05-24 21:55:44 +0530

    + Added buttons

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	ButtonScript[] buttonArray;
+
+	void Start() {
+		buttonArray = GameObject.FindObjectsOfType<ButtonScript> ();
+		foreach (ButtonScript thisButton in buttonArray) {
+			thisButton.GetComponent<SpriteRenderer>().color = Color.gray;
+		}
+	}
+
+	void OnMouseDown() {
+
+		foreach (ButtonScript thisButton in buttonArray) {
+			thisButton.GetComponent<SpriteRenderer>().color = Color.gray;
+		}
+
+		GetComponent<SpriteRenderer>().color = Color.white;
+	}
+}
