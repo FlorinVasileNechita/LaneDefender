f90a99d2d730a233e33320db84abaad08a0bd2f8
Siddhant Bansal <bansal.siddhant@gmail.com>
2015-05-24 19:53:30 +0530

    + Coded in Projectile behaviour ! There is a bug with Cactus projectiles not spawning, to be fixed.

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -2,18 +2,5 @@
 using System.Collections;
 
 public class Defender : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void OnTriggerEnter2D(Collider2D col) {
-		
-	}
+	// Nothing to see here.
 }
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject projectile, projectileParent, spawnLocation;
+
+	void Start () {
+		if (!projectile) {
+			Debug.LogError("Projectile not attached to " + name); 
+		}
+	}
+	
+	private void Fire() {
+		GameObject newProjectile = Instantiate(projectile, spawnLocation.transform.position, Quaternion.identity) as GameObject;
+		Debug.Log ("Spawning projectile " + projectile + " at " + spawnLocation.transform.position);
+		newProjectile.transform.parent = projectileParent.transform;
+	}
+}
